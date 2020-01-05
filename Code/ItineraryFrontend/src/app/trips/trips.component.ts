9e3626f19e324af00b75d4211c79631d6ab267d3
Adam Mirza <adammirza12@hotmail.com>
2020-01-05 03:05:20 -0800

    Scaffolding for listing trips (sample trips given)

--- Code/ItineraryFrontend/src/app/trips/trips.component.ts
+++ Code/ItineraryFrontend/src/app/trips/trips.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from '../create-trip/create-trip.component';
 
 @Component({
   selector: 'app-trips',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trips.component.css']
 })
 export class TripsComponent implements OnInit {
+  trips: Array<Trip>;
 
-  constructor() { }
+  constructor() {
+    this.trips = new Array<Trip>();
+  }
 
   ngOnInit() {
+    let tempTrip = new Trip();
+    tempTrip.title = "Test Trip 1";
+    tempTrip.description = "Test description here.";
+    tempTrip.address = "San Juan, Puerto Rico";
+    
+    this.trips.push(tempTrip);
+    
+
+    console.log(this.trips);
+
+    let tempTrip2 = new Trip();
+    tempTrip2.title = "Test Trip 2";
+    tempTrip2.description = "Test description here!!!";
+    tempTrip2.address = "Cape Town, South Africa";
+    
+    this.trips.push(tempTrip2);
+    console.log(this.trips);
   }
 
 }
